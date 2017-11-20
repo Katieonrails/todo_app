6907d6034727bb579a7a3e9bfcfd720b26e81873
Kasia <katieonrails@gmail.com>
2017-11-20 17:38:03 +0100

    websockets support

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  resources :lists
   devise_for :users
-  resources :todos
-  root "todos#index"
+  resources :todos do
+  	member do
+  		get :invert_checked
+  	end
+  end
+  root "lists#index"
+
+  mount ActionCable.server => '/cable'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
